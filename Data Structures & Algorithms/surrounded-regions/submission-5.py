d656c393792eb8e17ffe8780eff48eb4947f4971
Junhyeok Lee <98468722+born-june04@users.noreply.github.com>
2026-04-21 19:32:55 -0700

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,65 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ## get all the location of Os
+        ## do DFS -> meaning full search within that Os
+        ## change temp border Os to T
+        ## lastly if O -> X
+        ## and T -> O
+
+        rows = len(board)
+        cols = len(board[0])
+
+        # def dfs(r,c):
+        #     board[r][c] = "T"
+        #     for dr,dc in [(1,0),(0,1),(-1,0),(0,-1)]:
+        #         nr,nc = r+dr,c+dc
+        #         if 0<=nr<rows and 0<=nc<cols and board[nr][nc] == "O":
+        #             dfs(nr,nc)
+
+        # for r in range(rows):
+        #     for c in [0,cols-1]:
+        #         if board[r][c] == "O":
+        #             dfs(r,c)
+        
+        # for c in range(cols):
+        #     for r in [0, rows-1]:
+        #         if board[r][c] == "O":
+        #             dfs(r,c)
+        
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if board[r][c] == "O":
+        #             board[r][c] = "X"
+        #         elif board[r][c] == "T":
+        #             board[r][c] = "O"
+
+## board가 크면 dfs recursion이 터질 수 있음
+
+        from collections import deque
+        queue = deque()
+        for r in range(rows):
+            queue.append((r,0))
+            queue.append((r,cols-1))
+        for c in range(cols):
+            queue.append((0,c))
+            queue.append((rows-1,c))
+        
+        while queue:
+            r,c = queue.popleft()
+            if board[r][c] != "O":
+                continue
+            board[r][c] = "T"
+            for dr,dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+                nr,nc = r+dr,c+dc
+                if 0<=nr<rows and 0<=nc<cols and board[nr][nc] == "O":
+                    queue.append((nr,nc))
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "T":
+                    board[r][c] = "O"
